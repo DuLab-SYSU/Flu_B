7abeedcd51983873727134a8a3ff37a088552764
DuLab <76587472+DuLab-SYSU@users.noreply.github.com>
2025-04-30 10:07:32 +0800

    Update flu_b_ci.py

--- code/dynamic_model/flu_b_ci.py
+++ code/dynamic_model/flu_b_ci.py
@@ -31,31 +31,7 @@ numpyro.set_host_device_count(1)
 
 # ### Model
 # Compartment       Definition
-# S                 Susceptible individuals with no prior immunity
-# Iv                Infectious with BV; no prior immunity
-# Iy                Infectious with BY; no prior immunity
-# Rcv               Recoverd from BV; cross protected against BY
-# Rcy               Recoverd from BY; cross protected against BV
-# Rv                Recoverd from BV; no cross protection
-# Ry                Recoverd from BY; no cross protection
-# Jy                Infectious with BY; prior immunity to BV
-# Jv                Infectious with BV; prior immunity to BY
-# R                 Recoverd from BV and BY
-
-# Parameters        Definition
-# Nt                Populatin size
-# mu                Natural birth and death rate
-# beta              transmission rate
-# ksi               the relative transmission difference between Ii and Ji
-# alpha             the heterogeneous mixing effects on the spread of pathogen in the population
-# D                 infectious period
-# LV                Duration of BV specific immunity
-# LY                Duration of BY specific immunity
-# cV                the strength of partial cross protection from BV to BY
-# cY                the strength of partial cross protection from BY to BV
-# PV                the duration of cross protection to BY
-# PY                the duration of cross protection to BV
-# omega             the infectious period difference between Ii and Ji
+'''We will upload the complete code here once the manuscript is officially published'''
 
 def dz_dt(z, t, w_v, w_y, R0_v, R0_y, D, ksi, Lv, Ly, omega, cv, cy, immue_esp_pct_v1, immue_esp_pct_v2, immue_esp_pct_v3, immue_esp_pct_y1, immue_esp_pct_y2, npi_intensity, npi_start, npi_end, tv_0, ty_0, T, immue_esp_vt1, immue_esp_vt2, immue_esp_vt3, immue_esp_yt1, immue_esp_yt2, Pv, Py):
     
@@ -65,43 +41,7 @@ def dz_dt(z, t, w_v, w_y, R0_v, R0_y, D, ksi, Lv, Ly, omega, cv, cy, immue_esp_p
     S, Iv, Iy, Rcv, Rcy, Rv, Ry, Jv, Jy, R, _, _ = z
     N = S + Iv + Iy + Rcv + Rcy + Rv + Ry + Jv + Jy + R
 
-    # set import based on npi_intensity and epidemics
-    import_v = 2.
-    import_y = 2.
-
-    # seasonal forced transmission rate
-    sea_v = 1. + w_v * jnp.cos(2. * jnp.pi * (t - tv_0) / T)
-    sea_y = 1. + w_y * jnp.cos(2. * jnp.pi * (t - ty_0) / T)
-
-    # npi intensity
-    kappa = 1.
-
-    # infection ability, 不加import?
-    lambda_v = kappa * (R0_v / D) * sea_v * (Iv + ksi * Jv) / N
-    lambda_y = kappa * (R0_y / D) * sea_y * (Iy + ksi * Jy) / N
-
-    # escape population
-    esp_pct_v1 = jnp.where((jnp.abs(t - immue_esp_vt1) <= 1.), immue_esp_pct_v1, 0)
-    esp_pct_v2 = jnp.where((jnp.abs(t - immue_esp_vt2) <= 1.), immue_esp_pct_v2, 0)
-    esp_pct_v3 = jnp.where((jnp.abs(t - immue_esp_vt3) <= 1.), immue_esp_pct_v3, 0)
-    esp_pct_y1 = jnp.where((jnp.abs(t - immue_esp_yt1) <= 1.), immue_esp_pct_y1, 0)
-    esp_pct_y2 = jnp.where((jnp.abs(t - immue_esp_yt2) <= 1.), immue_esp_pct_y2, 0)
-    
-    # differential equations
-    dS_dt = -lambda_v * S - lambda_y * S + (Rv / Lv) + (Ry / Ly) + esp_pct_v1 * Rv + esp_pct_v2 * Rv + esp_pct_v3 * Rv + esp_pct_y1 * Ry + esp_pct_y2 * Ry - import_v - import_y 
-    dIv_dt = lambda_v * S - (Iv / D) + import_v
-    dIy_dt = lambda_y * S - (Iy / D) + import_y
-    dRcv_dt = (Iv / D) - (Rcv / Pv) - cv * lambda_y * Rcv
-    dRcy_dt = (Iy / D) - (Rcy / Py) - cy * lambda_v * Rcy
-    dRv_dt = (Rcv / Pv) + (R / Ly) - (Rv / Lv) - lambda_y * Rv - esp_pct_v1 * Rv - esp_pct_v2 * Rv - esp_pct_v3 * Rv + esp_pct_y1 * R + esp_pct_y2 * R
-    dRy_dt = (Rcy / Py) + (R / Lv) - (Ry / Ly) - lambda_v * Ry - esp_pct_y1 * Ry - esp_pct_y2 * Ry + esp_pct_v1 * R + esp_pct_v2 * R + esp_pct_v3 * R
-    dJv_dt = cy * lambda_v * Rcy + lambda_v * Ry - (Jv / (omega * D))
-    dJy_dt = cv * lambda_y * Rcv + lambda_y * Rv - (Jy / (omega * D))
-    dR_dt = (Jy / (omega * D)) + (Jv / (omega * D)) -(R / Ly) - (R / Lv) - esp_pct_v1 * R - esp_pct_v2 * R - esp_pct_v3 * R - esp_pct_y1 * R - esp_pct_y2 * R
-
-    # auxiliary variable -- new cases
-    dVnew_dt = lambda_v * S + import_v + cy * lambda_v * Rcy + lambda_v * Ry
-    dYnew_dt = lambda_y * S + import_y + cv * lambda_y * Rcv + lambda_y * Rv
+    '''We will upload the complete code here once the manuscript is officially published'''
     
     return jnp.stack([dS_dt, dIv_dt, dIy_dt, dRcv_dt, dRcy_dt, dRv_dt, dRy_dt, dJv_dt, dJy_dt, dR_dt, dVnew_dt, dYnew_dt])
 
@@ -137,53 +77,7 @@ def generate_data(S_rate_ini, Iv_rate_ini, Iy_rate_ini, Rcv_rate_ini, Rcy_rate_i
                   R0_v, R0_y, w_v, w_y, cv, cy, tv_0, ty_0, immue_esp_pct_v1, immue_esp_pct_v2, immue_esp_pct_v3, 
                   immue_esp_pct_y1, steps, npi_intensity):
 
-    # steps
-    M = steps
-    
-    # initial state proportions, not treated as parameters
-    N=100000.
-
-    S_ini   = N * S_rate_ini  
-    Iv_ini  = N * Iv_rate_ini  
-    Iy_ini  = N * Iy_rate_ini  
-    Rcv_ini = N * Rcv_rate_ini 
-    Rcy_ini = N * Rcy_rate_ini 
-    Rv_ini  = N * Rv_rate_ini  
-    Ry_ini  = N * Ry_rate_ini  
-    Jv_ini  = N * Jv_rate_ini  
-    Jy_ini  = N * Jy_rate_ini  
-    R_ini   = N * R_rate_ini   
-    z_init = jnp.array([S_ini, Iv_ini, Iy_ini, Rcv_ini, Rcy_ini, Rv_ini, Ry_ini, Jv_ini, Jy_ini, R_ini, 0.0, 0.0])
-
-
-    # measurement times
-    ts = jnp.arange(float(M+1))
-    
-    # R0_v = 1.3
-    # R0_y = 1.2
-    D  = 3.4 / 7.
-    ksi = 1.
-    Lv = 4. * 52
-    Ly = 4. * 52
-    Pv = (1./12) * 52
-    Py = (1./12) * 52
-    omega = 1.
-
-    # fixed parameters
-    mu = float(0.0098/52)
-    npi_start = 218.
-    npi_end = 301.
-    # npi_intensity = 1.
-    T = float(52.)
-    immue_esp_vt1 = float(101)
-    immue_esp_vt2 = float(205)
-    immue_esp_vt3 = float(309)
-    immue_esp_yt1 = float(205)
-    immue_esp_yt2 = float(260) 
-    # immue_esp_pct_v3 = 0.
-    # immue_esp_pct_y1 = 0.
-    immue_esp_pct_y2 = 0.
-
+    '''We will upload the complete code here once the manuscript is officially published'''
     # integrate dz/dt, z will have shape M x 10
     z = odeint(dz_dt, z_init, ts, w_v, w_y, R0_v, R0_y, D, ksi, Lv, Ly, omega, cv, cy, immue_esp_pct_v1, immue_esp_pct_v2, immue_esp_pct_v3, immue_esp_pct_y1, immue_esp_pct_y2, npi_intensity, npi_start, npi_end, tv_0, ty_0, T, immue_esp_vt1, immue_esp_vt2, immue_esp_vt3, immue_esp_yt1, immue_esp_yt2, Pv, Py, rtol=1e-6, atol=1e-5, mxstep=1000)
 
@@ -194,33 +88,7 @@ def params_estimate(S_rate_ini, Iv_rate_ini, Iy_rate_ini, Rcv_rate_ini, Rcy_rate
                     R0_v, R0_y, w_v, w_y, cv, cy, tv_0, ty_0, r, immue_esp_pct_v1, immue_esp_pct_v2, immue_esp_pct_v3, 
                     immue_esp_pct_y1, steps, npi_intensity, file_name):
     
-    ss = generate_data(S_rate_ini = S_rate_ini, 
-                       Iv_rate_ini = Iv_rate_ini, Iy_rate_ini = Iy_rate_ini, 
-                       Rcv_rate_ini = Rcv_rate_ini, Rcy_rate_ini = Rcy_rate_ini,
-                       Rv_rate_ini = Rv_rate_ini, Ry_rate_ini = Ry_rate_ini, 
-                       Jv_rate_ini = Jv_rate_ini, Jy_rate_ini = Jy_rate_ini, 
-                       R_rate_ini = R_rate_ini,
-                       R0_v = R0_v, R0_y = R0_y, 
-                       w_v = w_v, w_y = w_y, 
-                       cv = cv, cy = cy, 
-                       tv_0 = tv_0, ty_0 = ty_0,
-                       immue_esp_pct_v1 = immue_esp_pct_v1, 
-                       immue_esp_pct_v2 = immue_esp_pct_v2,
-                       immue_esp_pct_v3 = immue_esp_pct_v3,
-                       immue_esp_pct_y1 = immue_esp_pct_y1,
-                       npi_intensity = npi_intensity,
-                       steps = steps)
-    # new cases
-    inf_hat_v = r * jnp.diff(ss[:,10])
-    inf_hat_y = r * jnp.diff(ss[:,11])
-
-    df_ci = pd.DataFrame({f'inf_hat_v_{file_name}': inf_hat_v, f'inf_hat_y_{file_name}': inf_hat_y})
-
-    df_ci.to_csv(f"./result/dynamic_model/CI/data/case_{file_name}.csv", index=False)
-
-
-# In[10]:
-
+    '''We will upload the complete code here once the manuscript is officially published'''
 
 # import estimated params
 df_p = pd.read_csv(r'../data/B_params_rank.csv', index_col=0)
