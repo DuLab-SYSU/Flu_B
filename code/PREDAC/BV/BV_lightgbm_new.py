c052c761f98758f571eb0b712c6aa1d07519d40f
DuLab <76587472+DuLab-SYSU@users.noreply.github.com>
2025-04-30 09:49:38 +0800

    Update BV_lightgbm_new.py

--- code/PREDAC/BV/BV_lightgbm_new.py
+++ code/PREDAC/BV/BV_lightgbm_new.py
@@ -47,48 +47,11 @@ x_train, x_test, y_train, y_test = train_test_split(x_resampled, y_resampled, te
 print(y_train.value_counts())
 
 #tune params
-other_params = {'learning_rate':np.arange(0.01,0.2,0.01),
-                'n_estimators':list(range(10,1000,10)),
-                'max_depth':list(range(1,10,1)),
-                'min_child_weight':[1e-5,1e-4,1e-3,1e-2,1e-1,1,1e1,1e2,1e3,1e4],
-                'subsample':np.arange(0.8,1,0.1),
-                'colsample_bytree':np.arange(0.8,1,0.1),
-                'reg_alpha':[0,1e-5, 1e-2, 0.1, 1,2,3],
-                'reg_lambda':[0,1e-5, 1e-2, 0.1, 1,2,3],
-                'num_leaves':list(range(1,16,1)),
-                'min_child_samples':list(range(5,100,1)),
-                # 'lambda_l1': [1e-5,1e-3,1e-1,0.0,0.1,0.3,0.5,0.7,0.9,1.0],
-                # 'lambda_l2': [1e-5, 1e-3, 1e-1, 0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0],
-                'feature_fraction': [0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-                # 'bagging_fraction': [0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-                'bagging_freq': range(0,81,2)
-                }
-
-start = time.time()
-cv = KFold(n_splits=5,shuffle=True, random_state=seed).split(x_train)
-model = LGBMClassifier(random_state=seed)
-# optimized_lgb = GridSearchCV(estimator=model, param_grid =param_test1, scoring='accuracy',n_jobs=23, cv=cv)
-optimized_lgb = RandomizedSearchCV(estimator=model, param_distributions=other_params, n_iter=2000,scoring='roc_auc',n_jobs=30, cv=cv,random_state=seed)
-optimized_lgb.fit(x_train,y_train)
-print(optimized_lgb.best_params_)
-print(optimized_lgb.best_score_)
-end = time.time()
-print("The runing time of this process is {}min".format((end-start)/60))
+'''We will upload the complete code here once the manuscript is officially published'''
 
 # best params tuned by above steps
-best_params = {'learning_rate':0.03,
-                'n_estimators':670,
-                'max_depth':7,
-                'min_child_weight':0.001,
-                'subsample':0.9,
-                'colsample_bytree':0.9,
-                'reg_alpha':0.01,
-                'reg_lambda':3,
-                'num_leaves':13,
-                'min_child_samples':34,
-                'feature_fraction': 0.7,
-                'bagging_freq':4
-                }
+'''We will upload the complete code here once the manuscript is officially published'''
+
 # train LightGBM model on training dataset
 clf = LGBMClassifier(**best_params,random_state=seed)
 clf.fit(x_train,y_train)
