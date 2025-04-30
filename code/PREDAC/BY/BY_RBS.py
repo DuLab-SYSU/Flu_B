528847ab962e497b13d5367617eb99b6e103c519
DuLab <76587472+DuLab-SYSU@users.noreply.github.com>
2025-04-30 09:59:11 +0800

    Update BY_RBS.py

--- code/PREDAC/BY/BY_RBS.py
+++ code/PREDAC/BY/BY_RBS.py
@@ -25,17 +25,7 @@ df = pd.read_csv(r'../data/PREDAC/BY_model.csv',index_col=0)
 def acid_diff(str1,str2):
 
     '''find the RBS site'''
-
-    list_140loop = [i for i in range(136,144)]
-    list_190helix = [i for i in range(195, 205)]
-    list_240loop = [i for i in range(239, 245)]
-    global list_rbs
-    list_rbs = list_140loop + list_190helix +list_240loop
-    list_diff = []
-
-    for i in range(1,344):
-        if str1[i-1] != str2[i-1]:
-            list_diff.append(i)
+    '''We will upload the complete code here once the manuscript is officially published'''
     return list_diff
 
 BY_pdb = PandasPdb().read_pdb('../data/PREDAC/BY_template.pdb')
@@ -46,34 +36,7 @@ df_BY.reset_index(drop=True,inplace=True)
 def calEuclidean(str1,str2):
 
     '''calculate the average Euclidean Distance'''
-
-    list_eucfinal = []
-    list_diff = acid_diff(str1,str2)
-
-    if len(list_diff) == 0:
-        euc_change = 0
-
-    elif len(list_diff) != 0:
-
-        for i in list_diff:
-            list_region = list_rbs.copy()
-            arr_str1 = np.array([df_BY.loc[i-1,'x_coord'],df_BY.loc[i-1,'y_coord'],df_BY.loc[i-1,'z_coord']]) #注意索引从0开始
-            if i in list_region:
-                list_region.remove(i)
-            list_euc = []
-
-            for j in list_region:
-                arr_rbs = np.array([df_BY.loc[j-1,'x_coord'],df_BY.loc[j-1,'y_coord'],df_BY.loc[j-1,'z_coord']]) #第j个受体结合位点的坐标
-                euc = np.linalg.norm(arr_str1-arr_rbs)
-                list_euc.append(euc)
-            list_eucfinal.append(min(list_euc))
-
-        if len(list_eucfinal) <= 3:
-            euc_change = sum(list_eucfinal)/len(list_eucfinal)
-        elif len(list_eucfinal) > 3:
-            list_max3 = heapq.nlargest(3,list_eucfinal)
-            euc_change = sum(list_max3)/3
-
+    '''We will upload the complete code here once the manuscript is officially published'''
     return euc_change
 
 df['x_rbs'] = df.parallel_apply(lambda row: calEuclidean(row['virus1_seq'],row['virus2_seq']),axis=1)
