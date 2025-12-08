8f629e042d22d4b44ba87e6ffcb2146620585501
DuLab <76587472+DuLab-SYSU@users.noreply.github.com>
2025-12-08 16:11:59 +0800

    Add files via upload

--- code/dynamic_model/parameters_estimation/flu_b_mcmc_parallel_nc.py
+++ code/dynamic_model/parameters_estimation/flu_b_mcmc_parallel_nc.py
@@ -123,11 +123,11 @@ def dz_dt(z, t, w_v, w_y, R0_v, R0_y, D, ksi, Lv, Ly, omega, cv, cy, immue_esp_p
     lambda_y = kappa * (R0_y / D) * sea_y * (Iy + ksi * Jy) / N
 
     # escape population
-    esp_pct_v1 = jnp.where((jnp.abs(t - immue_esp_vt1) <= 1.), immue_esp_pct_v1, 0)
-    esp_pct_v2 = jnp.where((jnp.abs(t - immue_esp_vt2) <= 1.), immue_esp_pct_v2, 0)
-    esp_pct_v3 = jnp.where((jnp.abs(t - immue_esp_vt3) <= 1.), immue_esp_pct_v3, 0)
-    esp_pct_y1 = jnp.where((jnp.abs(t - immue_esp_yt1) <= 1.), immue_esp_pct_y1, 0)
-    esp_pct_y2 = jnp.where((jnp.abs(t - immue_esp_yt2) <= 1.), immue_esp_pct_y2, 0)
+    esp_pct_v1 = jnp.where((jnp.abs(t - immue_esp_vt1) <= 27.), immue_esp_pct_v1, 0)
+    esp_pct_v2 = jnp.where((jnp.abs(t - immue_esp_vt2) <= 27.), immue_esp_pct_v2, 0)
+    esp_pct_v3 = jnp.where((jnp.abs(t - immue_esp_vt3) <= 27.), immue_esp_pct_v3, 0)
+    esp_pct_y1 = jnp.where((jnp.abs(t - immue_esp_yt1) <= 27.), immue_esp_pct_y1, 0)
+    esp_pct_y2 = jnp.where((jnp.abs(t - immue_esp_yt2) <= 27.), immue_esp_pct_y2, 0)
     
     # differential equations
     dS_dt = -lambda_v * S - lambda_y * S + (Rv / Lv) + (Ry / Ly) + esp_pct_v1 * Rv + esp_pct_v2 * Rv + esp_pct_v3 * Rv + esp_pct_y1 * Ry + esp_pct_y2 * Ry - import_v - import_y  
